8e7c35eff877d457c56a4c1d675dc42bd2fe66aa
JulienMarseille <julien@squadracer.com>
2022-10-31 14:09:38 +0100

    insert values in grid

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,40 @@
 class GamesController < ApplicationController
     def index
+        session[:answers] = []
+        session[:rails_method] = random_rails_method.sample.to_s.upcase
         @answers = []
-        @rails_method = Array.public_instance_methods.grep(/[a-z_?]+/).sample.to_s.upcase
+        @rails_method = session[:rails_method]
     end
 
     def answer
-        Rails.logger.info(params)
-        @rails_method = params[:rails_method]
-        @answers = params[:answers].split
-        @answers << params[:answer].upcase
+        answer = params[:answer].upcase
+        session[:answers] << answer if is_a_valid_answer?(answer)
         respond_to do |format|
-            format.turbo_stream { render turbo_stream:
-                turbo_stream.replace('answers_grid', partial: 'games/answers_grid')
+            format.turbo_stream {
+                render turbo_stream:
+                    turbo_stream.replace(
+                        'game_turbo_frame',
+                        partial: 'games/game',
+                        locals: {
+                            rails_method: session[:rails_method],
+                            answers: session[:answers], 
+                        }
+                    )
             }
         end
+    end
+
+    private
+
+    def is_a_valid_answer?(answer)
+        answer.size == session[:rails_method].size && is_existing_method?(answer)
+    end
+
+    def is_existing_method?(answer)
+        random_rails_method.include?(answer)
+    end
+
+    def random_rails_method
+        @methods ||= Array.public_instance_methods.grep(/[a-z_?]+/).map { |method| method.to_s.upcase }
     end
 end
