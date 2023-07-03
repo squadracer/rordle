0a0366bda82c02286d112f69f14fd0e40faa2c4a
Sébastien Faure <41324885+fauresebast@users.noreply.github.com>
2023-07-03 13:35:19 +0200

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 echo "start.sh: installing doc"
-bundle install rdoc --all --ri --no-rdoc
+bundle exec rdoc --all --ri --no-rdoc
 
 echo "start.sh: starting server"
 bundle exec rails server
